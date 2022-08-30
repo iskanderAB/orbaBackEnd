b88ed7bc257030923c7939fe438d238263019c5a
guinoubi <mohamed.guinoubi@enicar.u-carthage.tn>
2022-08-30 15:49:10 +0200

    first commit

---
+++ entities/User.go
@@ -0,0 +1,17 @@
+package entities
+
+import "golang.org/x/text/date"
+
+type User struct {
+	ID       int16
+	Fullname string
+	Role     string
+	Email    string
+	Birthday string
+	Gender string
+	Telephone int32
+	Password int16
+
+
+
+}
